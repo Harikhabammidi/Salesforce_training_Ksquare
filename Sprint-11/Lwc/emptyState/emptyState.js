af2eb26574d2cc19693af4d51124701edf60036b
Harikhabammidi <harikhabammidi2005@gmail.com>
2026-08-10 18:22:57 +0530

    Create emptyState.js

---
+++ Sprint-11/Lwc/emptyState/emptyState.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+export default class EmptyState extends LightningElement {
+
+    @api title;
+    @api message;
+    @api actionLabel;
+
+    handleAction() {
+
+        this.dispatchEvent(
+            new CustomEvent('action')
+        );
+    }
+}
